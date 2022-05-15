2d4cf626852172e53c065f8b8479a1985d673a33
井上満喜 <inoue_mitsuki@MacBook-Air-im.local>
2022-05-15 19:09:07 +0900

    変数

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -8,8 +8,11 @@ class ComponentTestController extends Controller
 {
     //
     public function showComponent1(){
-        //view\testsフォルダのcomponent-test1ファイルを参照する
-        return view('tests.component-test1');
+        $message = 'aaa';
+        //view/testsフォルダのcomponent-test1ファイルを参照する
+        //compactでmessage上で定義したmessage関数を渡す
+        return view('tests.component-test1',
+        compact('message'));
     }
     public function showComponent2(){
         return view('tests.component-test2');
