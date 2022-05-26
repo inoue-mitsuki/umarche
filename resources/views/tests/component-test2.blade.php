60f68044a0ed3c19d919a7c11aaf841a25ef3d6b
井上満喜 <inoue_mitsuki@MacBook-Air-im.local>
2022-05-26 22:49:55 +0900

    クラスベース

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
         ヘッダー2
     </x-slot>
     component-test2
+    <x-test-class-base classBaseMessage='クラスベースメッセージです。'/>
+    <div class='mb-4'></div>
+    <x-test-class-base classBaseMessage='クラスベースメッセージです。' defaultMessage='初期値から変更しています。' />
 </x-tests.app>
